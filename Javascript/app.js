8d52334a7ee94cd08f59a920416b1750bb7558d5
151777 <151777@csgaugustinus.nl>
2019-11-19 19:59:25 +0000

    12

--- Javascript/app.js
+++ Javascript/app.js
@@ -5,3 +5,23 @@ window.onclick = function(event) {
     }
 }
 
+var audio = new Audio('http://zuniq.epizy.com/Hardest%20beat.mp3');
+        audio.volume = 1;
+        audio.autoplay = false;
+
+        $(document).ready(function(){
+        $('.trigger').click(function() {
+        if (audio.paused == false) {
+        audio.pause();
+        $('.fa-play').show();
+        $('.fa-pause').hide();
+        $('.music-card').removeClass('playing');
+        } else {
+        audio.play();
+        $('.fa-pause').show();
+        $('.fa-play').hide();
+        $('.music-card').addClass('playing');
+        }
+        });
+        });
+
